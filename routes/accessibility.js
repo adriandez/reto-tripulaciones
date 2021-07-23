1f362a26f3ca04bb0fca94aa62443b180979e5bf
Adrian E. de Zamacona Garza <adriandez@gmail.com>
2021-07-23 07:45:36 +0200

    added new table schemas and create update routes

--- routes/accessibility.js
+++ routes/accessibility.js
@@ -5,7 +5,12 @@ const items = require("../controllers/accessibility");
 router.get("/", routes.hello);
 router.post("/create", routes.createUser);
 router.post("/user", routes.findUser);
-
+router.get("/aseos", routes.getAseos);
+router.get("/aseos/:aseo_ID", routes.getByID);
+router.put("/aseos/raiting", routes.updateRaiting);
+router.post("/aseos/userRaiting", routes.ceateUserRaiting);
+router.put("/aseos/userRaiting", routes.updateUserRaiting);
+router.post("/seed", routes.seed);
 
 
 module.exports = router;
--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,9 @@
 const { Sequelize, DataTypes } = require("sequelize");
-const UserModel = require("../models/users");
+
+//const UserModel = require("../models/users");
+const AseoModel = require("../models/aseos");
+const RaitingModel = require("../models/raitings");
+const UserRaitingModel = require("../models/userRaitings");
 
 const sequelize = new Sequelize(
   process.env.MY_SQL_DATABASE,
@@ -8,17 +12,20 @@ const sequelize = new Sequelize(
   {
     dialect: "mysql",
     host: process.env.MY_SQL_HOST,
+    port: process.env.MY_SQL_PORT,
     pool: {
       max: 5,
       min: 0,
+      idle: 20000,
       acquire: 30000,
-      iddle: 10000,
+      evict: 15000,
     },
   }
 );
 
-const user = UserModel(sequelize, DataTypes);
+//const user = UserModel(sequelize, DataTypes);
+const aseo = AseoModel(sequelize, DataTypes);
+const raiting = RaitingModel(sequelize, DataTypes);
+const userRaiting = UserRaitingModel(sequelize, DataTypes);
 
-console.log(user);
-
-module.exports = { sequelize, user };
+module.exports = { sequelize, aseo, raiting, userRaiting };
