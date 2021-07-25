46efa909d6e8ab7eb6026f502e7192612014cea5
ignaciosein@gmail.com <ignaciosein@gmail.com>
2021-07-25 13:42:25 +0200

    menu footer con iconos creado

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,8 +1,26 @@
 import "./Footer.scss";
+import mapaMenu from  "../../img/mapaMenu.png";
+import profileMenu from  "../../img/profileMenu.png";
+import recordatorioMenu from  "../../img/recordatorioMenu.png";
+import resenasMenu from  "../../img/resenasMenu.png";
+
+
+
+
+
 
 const Footer = () => {
   return (
     <footer className="Footer">
+ 
+ <img src={mapaMenu}></img>
+ <img src={resenasMenu}></img>
+
+ <img src={recordatorioMenu}></img>
+ <img src={profileMenu}></img>
+
+ 
+      
     </footer>
   );
 };
