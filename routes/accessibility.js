6057a386b48c8912aacf6956f7c2981efac18ae2
Adrian E. de Zamacona Garza <adriandez@gmail.com>
2021-07-28 05:44:26 +0200

    added tutorial to backend

--- routes/accessibility.js
+++ routes/accessibility.js
@@ -7,12 +7,13 @@ router.post("/auth/login", routes.login);
 router.post("/auth/googleLogin", routes.googleLogin);
 router.post("/auth/posts", middleware.verifyToken, routes.posts);
 router.post("/auth/checkToken", routes.checkToken);
-router.post("/user",    routes.findUser);
-router.get("/aseos",    routes.getAseos);
+router.post("/user", routes.findUser);
+router.get("/aseos", routes.getAseos);
 router.get("/aseos/:aseo_ID", routes.getByID);
 router.put("/aseos/raiting", routes.updateRaiting);
 router.post("/aseos/userRaiting", routes.ceateUserRaiting);
 router.put("/aseos/userRaiting", routes.updateUserRaiting);
+router.put("/tutorial", routes.updateTutorial)
 router.post("/seed", routes.seed);
 
 
@@ -20,8 +21,6 @@ router.post("/seed", routes.seed);
 router.post("/user", middleware.verifyRoute, routes.findUser);
 router.get("/aseos", middleware.verifyRoute, routes.getAseos); */
 
- 
- 
 router.get("/aseos/details/:codigoAseo", routes.getByAseoID);
 router.get("/aseos/raiting/:codigoAseo", routes.getRatingWc);
 
