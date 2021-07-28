71435dd81e6dc17e2371f57a9c0510eb1984613a
Adrian E. de Zamacona Garza <adriandez@gmail.com>
2021-07-28 06:28:15 +0200

    added route header and middleware

--- routes/accessibility.js
+++ routes/accessibility.js
@@ -8,23 +8,15 @@ router.post("/auth/googleLogin", routes.googleLogin);
 router.post("/auth/posts", middleware.verifyToken, routes.posts);
 router.post("/auth/checkToken", routes.checkToken);
 router.post("/user", routes.findUser);
-router.get("/aseos", routes.getAseos);
+router.get("/aseos", middleware.verifyRoute, routes.getAseos);
 router.get("/aseos/:aseo_ID", routes.getByID);
-router.put("/aseos/raiting", routes.updateRaiting);
+router.put("/aseos/raiting", middleware.verifyRoute, routes.updateRaiting);
 router.post("/aseos/userRaiting", routes.ceateUserRaiting);
 router.put("/aseos/userRaiting", routes.updateUserRaiting);
-router.put("/tutorial", routes.updateTutorial)
+router.put("/tutorial", routes.updateTutorial);
 router.post("/seed", routes.seed);
-
-
-/* 
-router.post("/user", middleware.verifyRoute, routes.findUser);
-router.get("/aseos", middleware.verifyRoute, routes.getAseos); */
 
 router.get("/aseos/details/:codigoAseo", routes.getByAseoID);
 router.get("/aseos/raiting/:codigoAseo", routes.getRatingWc);
-
-
-
 
 module.exports = router;
